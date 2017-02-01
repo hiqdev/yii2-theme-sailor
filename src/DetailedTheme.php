8025739d8dbc9679f08d253245c2c913852e7849
tofid <andreyklochok@gmail.com>
2017-02-01 16:17:55 +0200

    Added theme files

---
+++ src/DetailedTheme.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tofid
+ * Date: 26.01.17
+ * Time: 17:23
+ */
+
+namespace hiqdev\themes\sailor;
+
+
+class DetailedTheme extends \hiqdev\thememanager\DetailedTheme
+{
+    /**
+     * @var string
+     */
+    public $type = 'site,blog';
+}
---
+++ src/Theme.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace hiqdev\themes\sailor;
+
+class Theme extends \hiqdev\thememanager\Theme
+{
+    public $label = 'Sailor';
+
+    public $assets = [
+        Asset::class,
+    ];
+}
