43fc89f5cbe438d999c3c7e5e4aa45901560ccc3
tofid <andreyklochok@gmail.com>
2017-02-01 16:23:25 +0200

    Configured main config

--- src/config/hisite.php
+++ src/config/hisite.php
@@ -1,3 +1,20 @@
 <?php
 
-return [];
+return [
+    'components' => [
+        'themeManager' => [
+            'sailor' => [
+                'class' => \hiqdev\themes\sailor\Theme::class,
+            ],
+        ],
+        'i18n' => [
+            'translations' => [
+                'hiqdev:themes:sailor' => [
+                    'class' => \yii\i18n\PhpMessageSource::class,
+                    'basePath' => '@hiqdev/themes/sailor/messages',
+                ],
+            ],
+        ],
+    ],
+];
+
