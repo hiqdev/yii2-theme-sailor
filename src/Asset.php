d611f8d0790e75c8c17719754ce2f74ec52e0ca6
tofid <andreyklochok@gmail.com>
2017-02-01 16:19:24 +0200

    Added main assets file

---
+++ src/Asset.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace hiqdev\themes\sailor;
+
+use yii\bootstrap\BootstrapPluginAsset;
+use yii\web\AssetBundle;
+
+/**
+ * Theme main asset bundle.
+ */
+class Asset extends AssetBundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $sourcePath = '@hiqdev/themes/sailor/assets';
+
+    /**
+     * {@inheritdoc}
+     */
+    public $css = [
+        'css/style.css',
+        'skins/default.css',
+        'bodybg/bg1.css',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public $js = [
+        'js/custom.js',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public $depends = [
+        BootstrapPluginAsset::class,
+    ];
+}
