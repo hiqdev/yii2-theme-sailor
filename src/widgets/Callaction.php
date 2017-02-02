a9126d6b3c786e6a846c9be390a84bad45eb241a
tofid <andreyklochok@gmail.com>
2017-02-02 17:38:59 +0200

    Added Callaction widget

---
+++ src/widgets/Callaction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace hiqdev\themes\sailor\widgets;
+
+use yii\base\Widget;
+
+class Callaction extends Widget
+{
+    public function run()
+    {
+        return $this->render('Callaction');
+    }
+}
---
+++ src/widgets/views/Callaction.php
@@ -0,0 +1,18 @@
+<section class="callaction">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8">
+                <div class="cta-text">
+                    <h2>Awesome site template <span>corporate</span> business</h2>
+                    <p> Etiam adipiscing, justo quis feugiat.Suspendisse eu erat quam. Vivamus porttitor eros quis nisi
+                        lacinia sed interdum</p>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="cta-btn">
+                    <a href="#" class="btn btn-theme btn-lg">Grab it now <i class="fa fa-angle-right"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
