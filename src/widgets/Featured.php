6b8acf496e014c522bdd11b3f0dcf22628a8a9d5
tofid <andreyklochok@gmail.com>
2017-02-02 17:38:32 +0200

    Added Featured widget

---
+++ src/widgets/Featured.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace hiqdev\themes\sailor\widgets;
+
+use yii\base\Widget;
+
+class Featured extends Widget
+{
+    public function run()
+    {
+        return $this->render('Featured');
+    }
+}
---
+++ src/widgets/views/Featured.php
@@ -0,0 +1,45 @@
+<section id="featured" class="bg">
+    <!-- start slider -->
+
+
+    <!-- start slider -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <!-- Slider -->
+                <div id="main-slider" class="main-slider flexslider">
+                    <ul class="slides">
+                        <li>
+                            <img src="img/slides/flexslider/1.jpg" alt=""/>
+                            <div class="flex-caption">
+                                <h3>Modern Design</h3>
+                                <p>Duis fermentum auctor ligula ac malesuada. Mauris et metus odio, in pulvinar urna</p>
+                                <a href="#" class="btn btn-theme">Learn More</a>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="img/slides/flexslider/2.jpg" alt=""/>
+                            <div class="flex-caption">
+                                <h3>Fully Responsive</h3>
+                                <p>Sodales neque vitae justo sollicitudin aliquet sit amet diam curabitur sed
+                                    fermentum.</p>
+                                <a href="#" class="btn btn-theme">Learn More</a>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="img/slides/flexslider/3.jpg" alt=""/>
+                            <div class="flex-caption">
+                                <h3>Clean & Fast</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit donec mer lacinia.</p>
+                                <a href="#" class="btn btn-theme">Learn More</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <!-- end slider -->
+            </div>
+        </div>
+    </div>
+
+
+</section>
