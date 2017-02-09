2c6b381d754085b8f3ad35bc922b577849b546e3
tofid <andreyklochok@gmail.com>
2017-02-09 21:11:36 +0200

    Fixed _content layout

--- src/views/layouts/_content.php
+++ src/views/layouts/_content.php
@@ -2,29 +2,38 @@
 
 use yii\widgets\Breadcrumbs;
 
+$isHomePage = Yii::$app->themeManager->isHomePage();
 ?>
-<?php if (!Yii::$app->themeManager->isHomePage() && empty($this->blocks['subHeader'])) : ?>
-    <section id="inner-headline">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <?= Breadcrumbs::widget([
-                        'homeLink' => ['label' => '<i class="fa fa-home"></i>', 'url' => '/'],
-                        'encodeLabels' => false,
-                        'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-                    ]) ?>
-                </div>
-            </div>
-        </div>
+
+<?php if ($isHomePage) : ?>
+    <?= $this->blocks['subHeader'] ?>
+    <section id="content">
+        <?= $content ?>
     </section>
 <?php else: ?>
-    <section class="callaction">
-        <?= $this->blocks['subHeader'] ?>
+    <?php if ($this->params['breadcrumbs']) : ?>
+        <section id="inner-headline">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <?= Breadcrumbs::widget([
+                            'homeLink' => ['label' => '<i class="fa fa-home"></i>', 'url' => '/'],
+                            'encodeLabels' => false,
+                            'links' => !empty($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+                        ]) ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+    <?php endif ?>
+    <?= $this->blocks['subHeader'] ?>
+    <section id="content">
+        <div class="container">
+            <?= $content ?>
+        </div>
     </section>
-<?php endif ?>
-<section id="content">
-    <div class="container">
-        <?= $content ?>
-    </div>
-</section>
+<?php endif; ?>
+
+
+
 
